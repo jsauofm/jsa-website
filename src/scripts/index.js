471d91d9c4cfb67a3926756b043da62b50367c34
Orbital-Web <rmeguro@umich.edu>
2024-02-03 18:19:51 -0500

    test: json loading

---
+++ src/scripts/index.js
@@ -0,0 +1,16 @@
+import boardies from '../../Data/Boardies/Boardies';
+
+load_slideshow();
+load_boardies();
+
+
+// load slideshow images
+function load_slideshow() {
+
+}
+
+
+// load boardie info
+function load_boardies() {
+    console.log(boardies);
+}
