24514601d65fcf09d2f992a9fba00727a3690005
Orbital-Web <36625832+Orbital-Web@users.noreply.github.com>
2024-03-08 18:52:05 -0500

    feat: dynamic main page generation

---
+++ Data/slideshow.js
@@ -0,0 +1,8 @@
+// always include 5 photos
+slideshows = [
+    "src/photos/festifall.JPG",
+    "src/photos/mjsa-group.JPG",
+    "src/photos/kakigori-kickoff.JPG",
+    "src/photos/ags-table.JPG",
+    "src/photos/festifall.JPG"
+]
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,16 +1,54 @@
-import boardies from '../../Data/Boardies/Boardies.json';
+function generate_index() {
+    // generate boardie info
+    console.log(boardie_info);
+    let boardies = document.getElementById("boardies");
 
-load_slideshow();
-load_boardies();
+    for (let i=0; i<boardie_info.length; i++) {
+        const boardie = boardie_info[i];
 
+        let boardie_card = document.createElement("div");
+        boardie_card.className = "card"
 
-// load slideshow images
-function load_slideshow() {
+        // photo
+        boardie_card.innerHTML += '<img src="' + boardie.photo +
+        '" alt=' + boardie.name + ' class="card__image">';
 
-}
+        // info
+        boardie_card.innerHTML += '<p class="card__name">' + boardie.name + '<br>' + boardie.position + '</p>';
+        boardie_card.innerHTML += '<p><b>Year: </b>' + boardie.year +
+        '<br><b>Major: </b>' + boardie.major + '<br><b>Interests: </b>' + boardie.interests + '</p>';
 
+        // contacts
+        let boardie_contact = document.createElement("p");
+        for (contact in boardie.contacts) {
+            let c_span = document.createElement("span");
+            c_span.className = "fa-stack fa-lg>";
+            let c_bg = document.createElement("i");
+            c_bg.className = "fa fa-circle fa-stack-2x icon-background";
+            let c_a = document.createElement("a");
+            c_a.href = boardie.contacts[contact];
+            let c_icon = document.createElement("i");
+            c_icon.className = "fa fa-stack-1x";
+            if (contact == "email")
+                c_icon.classList.add("fa-envelope");
+            else if (contact == "facebook")
+                c_icon.classList.add("fa-facebook");
+            else if (contact == "instagram")
+                c_icon.classList.add("fa-instagram");
+            else if (contact == "linkedin")
+                c_icon.classList.add("fa-linkedin");
+            c_a.appendChild(c_icon);
+            c_span.appendChild(c_bg);
+            c_span.appendChild(c_a);
+            boardie_contact.appendChild(c_span);
+        }
+        boardie_card.appendChild(boardie_contact);
+        boardies.appendChild(boardie_card);
+    }
 
-// load boardie info
-function load_boardies() {
-    console.log(boardies);
+    // generate slideshow
+    let slideshow = document.getElementById("slideshow");
+    for (let i=0; i<slideshows.length; i++)
+        slideshow.innerHTML += '<img src="' + slideshows[i] + '">';
+
 }
