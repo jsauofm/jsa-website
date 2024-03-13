3e37557bfe3b6070da44a6524c4d68380e3ba301
Orbital-Web <rmeguro@umich.edu>
2024-03-12 23:31:35 -0400

    refactor: fixed indentation

--- Data/slideshow.js
+++ Data/slideshow.js
@@ -1,6 +1,6 @@
 // always include 4 photos
 slideshows = [
-    "../Data/slideshows/mass_meeting.jpg",
+    "../Data/slideshows/mass_meeting.JPG",
     "../Data/slideshows/origami.jpg",
     "../Data/slideshows/karaoke.jpg",
     "../Data/slideshows/undoukai.jpg",
